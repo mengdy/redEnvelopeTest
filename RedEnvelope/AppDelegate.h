ae1ec198a2f331b8197c418bb696b4ec1ce45bf9
孟凡 <DYA1227@163.com>
2017-04-10 11:11:38 +0800

    Initial Commit

---
+++ RedEnvelope/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RedEnvelope
+//
+//  Created by mengdy on 17/4/10.
+//  Copyright © 2017年 mengdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
